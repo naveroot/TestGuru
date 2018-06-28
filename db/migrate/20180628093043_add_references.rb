0ed52a83f56e3a91412fcab66052b8300978ee7b
naveroot <naveroot@gmail.com>
2018-06-28 12:44:39 +0300

    ls2

---
+++ db/migrate/20180628093043_add_references.rb
@@ -0,0 +1,7 @@
+class AddReferences < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :answers, :question, foreign_key: true, index: true
+    add_reference :questions, :test, foreign_key: true, index: true
+    add_reference :tests, :category, foreign_key: true, index: true
+  end
+end
