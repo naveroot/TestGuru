63b41884b7a664f913340f39f17b393a0ff94d84
naveroot <naveroot@gmail.com>
2018-07-03 12:29:23 +0300

    create join table rework

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
+  def tests_with_level(level)
+    Test.joins(:tests_users).where(level: level, tests_users: {user_id: id})
+  end
 end
