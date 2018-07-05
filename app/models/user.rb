b9575685c50f97d112fb0dce8d0eff2576e1ca51
naveroot <naveroot@gmail.com>
2018-07-05 16:39:02 +0300

    ls4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :tests
+  has_many :tests_users
+  has_many :tests, through: :tests_users
   def tests_with_level(level)
     Test.joins(:tests_users).where(level: level, tests_users: {user_id: id})
   end
