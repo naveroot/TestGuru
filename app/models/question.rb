0bb7b01764df05ec3bfdec750e73bb83b1c1592f
naveroot <naveroot@gmail.com>
2018-07-09 12:27:21 +0300

    ls4

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
+
+  validates :body, presence: true
+  validate :validate_answers_count
+  scope :correct, -> { where(correct: true).order(created_at: :desc) }
+
+
+  def validate_answers_count
+    errors.add(:question_id, 'must have 1..4 answers') unless answers.count.between?(1, 4)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
+
+  validates :email, presence: true, uniqueness: true
+
   def tests_with_level(level)
-    Test.joins(:tests_users).where(level: level, tests_users: {user_id: id})
+    tests.where(level: level)
   end
-
-  # def tests_with_level(level)
-  #   Test.where(level: level, tests_users: {user_id: id})
-  #
-  #   2.4.1 :001 > user = User.first
-  #   User Load (0.2ms)  SELECT  "users".* FROM "users" ORDER BY "users"."id" ASC LIMIT ?  [["LIMIT", 1]]
-  #   => #<User id: 1, name: "User1", created_at: "2018-07-03 08:57:47", updated_at: "2018-07-03 08:57:47">
-  #   2.4.1 :002 > user.tests_with_level 1
-  #   Test Load (0.5ms)  SELECT  "tests".* FROM "tests" WHERE "tests"."level" = ? AND "tests_users"."user_id" = ? LIMIT ?  [["level", 1], ["user_id", 1], ["LIMIT", 11]]
-  #   ActiveRecord::StatementInvalid: SQLite3::SQLException: no such column: tests_users.user_id: SELECT  "tests".* FROM "tests" WHERE "tests"."level" = ? AND "tests_users"."user_id" = ? LIMIT ?
-  # end
-
 end
