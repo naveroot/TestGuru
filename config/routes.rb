947211a9ed6404fef784794eccd07bc0118cd0af
naveroot <naveroot@gmail.com>
2018-07-19 16:21:36 +0300

    ls6

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :questions
-  resources :tests
+
+  resources :tests do
+    resources :questions, shallow: true
+  end
   root to: 'tests#index'
 end
