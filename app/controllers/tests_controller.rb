eb9cb5111757dd2c8d0699873da86ba40d8258cb
naveroot <naveroot@gmail.com>
2018-08-20 15:40:58 +0300

    after 2 screencast

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,13 +3,13 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_test, only: %i[show destroy edit update start]
-  before_action :find_user, only: :start
+
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_test_not_found
 
   def start
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   def index
@@ -48,10 +48,6 @@ class TestsController < ApplicationController
   end
 
   private
-
-  def find_user
-    @user = User.first
-  end
 
   def find_test
     @test = Test.find(params[:id])
