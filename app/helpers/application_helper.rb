773e833bcef75f77dbeed6b7f16388c6e470c165
naveroot <naveroot@gmail.com>
2018-08-29 10:09:24 +0300

    after review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  BOOTSTRAP_ALERT_CLASSES = { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', notice: 'alert-info' }.freeze
   def current_year
     Date.current.year
   end
@@ -7,13 +10,16 @@ module ApplicationHelper
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: :blank
   end
 
-  def bootstrap_class_for_flash(flash_type)
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  def flash_messages
+    tags = flash.map do |key, value|
+      content_tag(:p, value, class: "flash #{bootstrap_class_for_flash(key)}")
+    end
+    safe_join(tags)
   end
 
-  def flash_alerts
-    massages = ''
-    flash.each { |key, value| massages << content_tag(:p, value, class: "flash #{bootstrap_class_for_flash(key)}") }
-    massages.html_safe
+  private
+
+  def bootstrap_class_for_flash(flash_type)
+    BOOTSTRAP_ALERT_CLASSES[flash_type.to_sym] || flash_type
   end
 end
