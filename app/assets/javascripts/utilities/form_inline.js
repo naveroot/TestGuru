7283f0df3ad789eff123487efe0d3cf447a8d941
naveroot <naveroot@gmail.com>
2018-09-03 18:05:55 +0300

    progress bar

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
-    var controls = document.querySelectorAll('.form-inline-link')
+    var controls = document.querySelectorAll('.form-inline-link');
 
     if (controls.length) {
         for (var i = 0; i < controls.length; i++) {
@@ -7,26 +7,25 @@ document.addEventListener('turbolinks:load', function() {
         }
     }
 
-    var errors = document.querySelector('.resource-errors')
-    var control = document.querySelector('.form-inline-link')
+    var errors = document.querySelector('.resource-errors');
+    var control = document.querySelector('.form-inline-link');
     if (errors && control) {
-        var resourceId = errors.dataset.resourceId
+        var resourceId = errors.dataset.resourceId;
         formInlineHandler(resourceId)
     }
-
-})
+});
 
 function formInlineLinkHandler(event) {
-    event.preventDefault()
-    var testId = this.dataset.testId
+    event.preventDefault();
+    var testId = this.dataset.testId;
     formInlineHandler(testId)
 }
 
 function formInlineHandler(testId) {
     var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]');
     var rejectLink = document.querySelector('.form-inline-reject-link[data-test-id="' + testId + '"]');
-    var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-    var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+    var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]');
+    var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]');
 
     if (formInline.classList.contains('hide')) {
         testTitle.classList.add('hide');
@@ -34,8 +33,8 @@ function formInlineHandler(testId) {
         rejectLink.classList.remove('hide');
         link.classList.add('hide');
     } else {
-        testTitle.classList.remove('hide')
-        formInline.classList.add('hide')
+        testTitle.classList.remove('hide');
+        formInline.classList.add('hide');
         rejectLink.classList.add('hide');
         link.classList.remove('hide');
     }
