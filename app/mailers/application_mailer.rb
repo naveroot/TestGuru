5204ed54ff8a74bb0f1b4b7896ccfc6ac36afd22
naveroot <naveroot@gmail.com>
2018-09-05 18:28:45 +0300

    add locales fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: %{'TestGuru' <mailer@testguru.com}
+  default from: %{'TestGuru' <mailer@testguru.com>}
   layout 'mailer'
 end
