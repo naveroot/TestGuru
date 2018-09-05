b7f745674509af271a9cee85122b133e1a2a6d2e
naveroot <naveroot@gmail.com>
2018-09-05 19:22:26 +0300

    add locales fix

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -7,7 +7,7 @@ class FeedbacksMailer < ApplicationMailer
     @feedback = feedback
     @recipients = @admins.map(&:email).join(',')
     mail  to: @recipients,
-          subject: "I18n.t('mail.feedback.subject')} #{@feedback.user.first_name} #{@feedback.user.last_name}"
+          subject: "#{I18n.t('mail.feedback.subject')} #{@feedback.user.first_name} #{@feedback.user.last_name}"
   end
 
   private
